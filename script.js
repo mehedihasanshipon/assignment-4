8d97a4320ee9098fe2e6435108ece38cb0bbf5ca
mehedihasanshipon <siponmahmud112255@gmail.com>
2021-01-30 03:59:39 +0600

    Assignment 4 completed

--- script.js
+++ script.js
@@ -19,17 +19,31 @@ function ticketQuantityChange(isIncrease,ticketClassName){
 function totalCalculate(){
     const firstClassCount = getInput('firstClassTicket');
     const economyClassCount = getInput('economyClassTicket');
+    const totalTicket = firstClassCount + economyClassCount;
     const totalPrice = firstClassCount * 150 + economyClassCount * 100;
     document.getElementById('subTotal').innerText = '$' + totalPrice;
     const tax = Math.round(totalPrice * 0.1);
     document.getElementById('tax-amount').innerText = '$' + tax;
     const grandTotal = totalPrice + tax;
     document.getElementById('grand-total').innerText = '$' + grandTotal;
+    document.getElementById('firstClassQuantity').innerText = firstClassCount;
+    document.getElementById('firstClassPrice').innerText = firstClassCount * 150;
+    document.getElementById('economyClassQuantity').innerText = economyClassCount;
+    document.getElementById('economyClassPrice').innerText = economyClassCount * 100;
+    document.getElementById('totalTicket').innerText = totalTicket;
+    document.getElementById('totalTicketPrice').innerText = totalPrice;
 }
 
-// Get input function
+// Get input value function
 function getInput(id){
     const ticketInput = document.getElementById(id);
     const ticketCount = parseInt(ticketInput.value);
     return ticketCount;
 }
+//Ticketing area and confirm-area display none and block section
+document.getElementById('bookButton').addEventListener('click',function(){
+    const ticketArea = document.getElementById('ticket-area');
+    ticketArea.style.display = 'none';
+    const confirmArea = document.getElementById('confirm-area');
+    confirmArea.style.display = 'block';
+})
